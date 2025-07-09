f89ed7e84655f93d1915fb600435d1e46e386b9b
Gordey <112198400+Finyi@users.noreply.github.com>
2025-07-09 19:59:45 +0300

    Add files via upload

---
+++ js/themeButton.js
@@ -0,0 +1,10 @@
+let switchMode = document.getElementById("themeButton");
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("theme")
+    if (theme.getAttribute("href") == "css/style.css") {
+        theme.href = "css/style2.css"
+    } else {
+        theme.href = "css/style.css"
+    }
+}
