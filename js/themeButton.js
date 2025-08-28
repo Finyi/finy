53266a0a0b6bb7adcbaeea5a13401e6c38878e7e
Gordey <112198400+Finyi@users.noreply.github.com>
2025-08-28 16:09:44 +0300

    Delete js directory

--- js/themeButton.js
+++
@@ -1,9 +0,0 @@
-let switchMode = document.getElementById("themeButton");
-switchMode.onclick = function () {
-    let theme = document.getElementById("theme")
-    if (theme.getAttribute("href") == "css/style.css") {
-        theme.href = "css/style2.css"
-    } else {
-        theme.href = "css/style.css"
-    }
-}
